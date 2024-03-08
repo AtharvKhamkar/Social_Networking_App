3c3b4797036e1c6f4a3379a8b32190220af5af43
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-03-08 16:01:41 +0530

    add clodinary upload & jwt token functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,12 +2,11 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, userName, email, password, bio, } = req.body
-
-    console.log(req.body);
     
     if (
         [name, userName, email, password, bio].some((field) => 
@@ -24,13 +23,20 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(401,"user with email or username already exists")
     }
 
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let [avatar,coverImage] = await Promise.all([uploadOnCloudinary(avatarLocalPath),uploadOnCloudinary(coverImageLocalPath)])
+
     const user = await User.create({
         name,
         userName:userName.toLowerCase(),
         email,
         password,
         bio,
-        posts:[]
+        posts: [],
+        avatar: avatar?.url,
+        coverImage:coverImage?.url
     })
 
     const createdUser = await User.findById(user._id).select("-password")
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
-import mongoose, { Schema } from "mongoose"
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
 
 
 const userSchema = new Schema({
@@ -28,8 +29,11 @@ const userSchema = new Schema({
         type: String,
         required: [true,"password is required"],
     },
-    profilePicture: {
+    avatar: {
         type: String
+    },
+    coverImage: {
+        type:String
     },
     refreshToken: {
         type:String
@@ -43,6 +47,39 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            name:this.name
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
 
 
 
