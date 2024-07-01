1bb884694df48936d3b02cbd3cdca50fe9dae8ec
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-07-01 11:44:11 +0530

    add bio missing field while creating user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,6 +30,9 @@ const userSchema = new Schema({
         type: String,
         required: [true,"password is required"],
     },
+    bio: {
+        type:String,
+    },
     posts: [
         {
             type: Schema.Types.ObjectId,
